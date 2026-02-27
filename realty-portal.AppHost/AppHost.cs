7d9e9c36997207448e4c327c9976a0c2d50f4cd3
Dominik Smola <dsmola9@gmail.com>
2026-02-27 15:13:20 +0100

    Add aspire

---
+++ realty-portal.AppHost/AppHost.cs
@@ -0,0 +1,21 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+// 1. Define external resources (Aspire will run these in Docker automatically)
+var redis = builder.AddRedis("cache");
+var postgres = builder.AddPostgres("postgres")
+    .WithPgAdmin() // Adds a UI for your DB
+    .WithDataVolume("realty-portal-data"); // Persists data across restarts
+var db = postgres.AddDatabase("sqldata");
+
+// 2. Define your Server (API)
+var api = builder.AddProject<Projects.Server>("backend-api")
+    .WithReference(db)
+    .WithReference(redis)
+    .WithExternalHttpEndpoints();
+
+// 3. Define your Blazor WASM Client
+var client = builder.AddProject<Projects.Client>("client")
+    .WithReference(api)
+    .WithExternalHttpEndpoints();
+
+builder.Build().Run();
